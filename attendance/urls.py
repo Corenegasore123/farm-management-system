8ceebfd24201764f2454b7b0c032317613f68487
Corenegasore123 <corenegasore@gmail.com>
2025-10-21 08:08:18 +0200

    chore(ui): hide Attendance and make Farmers the default landing
    - Remove Attendance link from navbar
    - Set /attendance/ to show Farmers list
    - Keep attendance models/data intact (UI-only removal)

--- attendance/urls.py
+++ attendance/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import add_farmer, attendance_list, students_list, add_student, farmers_list
 
 urlpatterns = [
-    path('', attendance_list, name='attendance_list'),
+    path('', farmers_list, name='farmers_list_default'),
     path('farmers/add/', add_farmer, name='add_farmer'),
     path('farmers/', farmers_list, name='farmers_list'),
     path('students/', students_list, name='students_list'),
